a44114c5f8f0ccbdf73466f77bbfd91ec6fc7999
Kamren Northrop <nkamren@outlook.com>
2026-04-13 12:38:50 -0700

    Security features for user input, lower bound range, file ingestion

--- Capstone - Linear Regression/Main.py
+++ Capstone - Linear Regression/Main.py
@@ -10,7 +10,7 @@ def main():
     #Clean data
     model.clean_data()
 
-    #Process data for encoding
+    #Process data
     model.preprocess_data()
 
     #Split Data
@@ -21,10 +21,92 @@ def main():
 
     #Evaluate
     r2, mae = model.evaluate_model()
-    print(f"R2 is {r2:.3f}")
-    print(f"MAE is {mae:.2f}")
+    print(f"R2 : {r2:.3f}")
+    print(f"MAE : {mae:.2f}")
 
-    
+    #Get user data to make a prediction
+    making_prediction = True
+    while making_prediction:
+        while True:
+            try:
+                age = int(input("What is your age? "))
+                if age <= 0:
+                    print("Please enter a valid age")
+                    continue
+                break
+            except ValueError:
+                print("Please enter a valid numeric age")
+
+        while True:
+            try:
+                bmi = float(input("What is your BMI? "))
+                if bmi <= 0:
+                    print("Please enter a valid BMI")
+                    continue
+                break
+            except ValueError:
+                print("Please enter a valid numeric BMI")
+
+        while True:
+            try:
+                children = int(input("How many children do you have? "))
+                if children < 0:
+                    print("Please enter 0 or a positive integer")
+                    continue
+                break
+            except ValueError:
+                print("Please enter a valid numeric integer for children")
+
+        while True:
+            sex = input("Are you male or female? (male/female) ").lower()
+            valid_input_for_sex = ["male", "female"]
+            if sex not in valid_input_for_sex:
+                print("Please enter either male or female")
+                continue
+            break
+
+        while True:
+            smoking_status = input("Do you smoke? (yes/no) ").lower()
+            valid_input_for_smoking = ["yes", "no"]
+            if smoking_status not in valid_input_for_smoking:
+                print("Please enter either yes or no.")
+                continue
+            break
+
+        while True:
+            region = input("What region are you from? (NW, NE, SW, SE) ").lower()
+            valid_input_for_region = ["nw", "ne", "sw", "se"]
+            if region not in valid_input_for_region:
+                print("Please enter a valid region: nw, ne, sw, se.")
+                continue
+            break
+
+        outcome = model.make_prediction(age, bmi, children, sex, smoking_status, region)
+        if outcome is None:
+            print("No prediction made, try again")
+            continue
+
+        #Calcuate upper and lower range considering the MAE
+        lower_range = outcome - mae
+        if lower_range < 0:
+            lower_range = 0
+        upper_range = outcome + mae
+
+        #Print prediction and the range
+        print(f"Your predicted annual cost: ${outcome:.2f}\n")
+        print(f"Your predicted annual cost range considering the MAE:\nLower bound: ${lower_range:.2f} \nUpper bound: ${upper_range:.2f}")
+        model.describe_stats()
+        model.prediction_range_plot(outcome, lower_range, upper_range)
+        model.bmi_plot()
+        model.smoker_vs_charges_bar_chart()
+
+        #Does the user want to make another prediction?
+        accepted = ["yes", "y"]
+        again = input("\nWould you like to make another prediction? (yes/no) ").lower()
+        if again.lower() in accepted:
+            continue
+        else:
+            break
 
 if __name__ == "__main__":
     main()
--- Capstone - Linear Regression/linear_regression.py
+++ Capstone - Linear Regression/linear_regression.py
@@ -1,7 +1,8 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression as SkLinearRegression
-from sklearn.metrics import r2_score, mean_absolute_error
+from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error
+import matplotlib.pyplot as plot
 
 class LinearRegression:
     def __init__(self):
@@ -14,11 +15,17 @@ class LinearRegression:
         self.y_train = None
         self.model = None
 
+    #IMPORTANT: Update your file path here!!!!
     def load_data(self):
         #Load data into DF
         file_path = r"C:\Users\nkamr\OneDrive\Desktop\insurance.csv"
-        self.df = pd.read_csv(file_path)
-        return self.df
+
+        try:
+            self.df = pd.read_csv(file_path)
+            return self.df
+        except FileNotFoundError:
+            print("File was not found. Please check the file path and try again.")
+            return None
     
     def clean_data(self):
         if self.df is None:
@@ -36,7 +43,7 @@ class LinearRegression:
             print("No Data Loaded")
             return None
         
-        #separate indexes from charges
+        #separate features from charges
         self.x = self.df.drop(columns=[target_column])
         self.y = self.df[target_column]
 
@@ -48,6 +55,7 @@ class LinearRegression:
             print("Please preprocess the data")
             return None
         
+        #Split: 80% train, 20% test
         self.x_train, self.x_test, self.y_train, self.y_test = train_test_split(self.x, self.y, test_size=test_size, random_state=42, shuffle=True)
 
     def train_model(self):
@@ -69,9 +77,92 @@ class LinearRegression:
             print("Please preprocess and split data")
             return None
         
+        #Make test prediction
         pred = self.model.predict(self.x_test)
 
+        #Target: R2 > 0.7, MAE < 5000
         r2 = r2_score(self.y_test, pred)
         mae = mean_absolute_error(self.y_test, pred)
 
         return r2, mae
+
+    def make_prediction(self, age, bmi, children, sex, smoking_status, region):
+        if self.model is None:
+            print("Model is not  trained")
+            return None
+        
+        user_data = {
+            "age" : age,
+            "bmi" : bmi,
+            "children" : children,
+            "sex" : sex.lower(),
+            "smoker" : smoking_status.lower(),
+            "region" : region.lower()
+        }
+
+        #Create new dataframe with user data
+        user_df = pd.DataFrame([user_data])
+        user_df = pd.get_dummies(user_df, drop_first=True)
+
+        #Fill in possible empty columns
+        for col in self.x.columns:
+            if col not in user_df.columns:
+                user_df[col] = 0
+
+        user_df = user_df[self.x.columns]
+
+        pred = self.model.predict(user_df)
+        return pred[0]
+    
+    def describe_stats(self):
+        print(self.df.describe())
+    
+    def prediction_range_plot(self, prediction, lower, upper):
+        if self.y is None:
+            print("Data is not available")
+            return None
+        
+        if lower < 0:
+            lower = 0
+        
+        plot.figure()
+
+        #Show line for: prediction, upperbound, and lower bound compared to the actual data
+        plot.hist(self.y, bins=15)
+        plot.axvline(prediction, color="green", linestyle='dashed', linewidth=3, label="Prediction")
+        plot.axvline(lower, color="red", linestyle="dashed",linewidth=3, label="Lower Bound")
+        plot.axvline(upper, color="red", linestyle="dashed",linewidth=3, label="Upper Bound")
+
+        plot.title("Your Prediction vs Real Data")
+        plot.xlabel("Charges")
+        plot.ylabel("Frequency")
+        plot.show()
+
+    def bmi_plot(self):
+        if self.df is None:
+            print("Data not loaded")
+            return None
+        
+        bmi_groups = pd.cut(self.df["bmi"], bins=6)
+        average = self.df.groupby(bmi_groups)["charges"].mean()
+
+        plot.figure()
+        average.plot(kind="line", marker="X", color="red")
+        plot.title("Average Charges By BMI Range")
+        plot.xlabel("BMI Range")
+        plot.ylabel("Average Charges")
+        plot.show()
+
+    def smoker_vs_charges_bar_chart(self):
+        if self.df is None:
+            print("Data not loaded")
+            return None
+        
+        avg_charges = self.df.groupby("smoker")["charges"].mean()
+
+        plot.figure()
+        avg_charges.plot(kind="bar")
+        plot.title("Average Insurance Charges By Smoking Status")
+        plot.xlabel("Smoking status")
+        plot.ylabel("Average Charges")
+        plot.show()
